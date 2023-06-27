5a9e38317ff99e2443bc2bef520ce1c1401c0540
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-06-27 19:20:20 +0530

    Register comment in Admin

--- blog_backend/blogapp/admin.py
+++ blog_backend/blogapp/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Comments
+
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ("title", "author")
     search_fields = ["title"]
     list_filter = ["date_created"]
 
-admin.site.register(Blog, BlogAdmin)
 
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ["commenter", "blog","comment"]
+    search_fields = ["commenter"]
+    list_filter = ["created_at"]
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comments, CommentsAdmin)
