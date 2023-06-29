518b25dc75a0143698583bd2a7ac9587514678a0
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-06-29 12:27:21 +0530

    Add contactapp

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "blogapp",
+    "contact",
     "corsheaders",
     "rest_framework_simplejwt",
     "social_auth",
