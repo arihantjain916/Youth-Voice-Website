332cb3d51ac105de385ad5317da928340e900d5a
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-07-05 14:21:31 +0530

    Delete blog_frontend/blog_backend directory

--- blog_frontend/blog_backend/social_auth/admin.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-from django.contrib import admin
-
-=======
-from django.contrib import admin
-
->>>>>>> dae3be574253c406cd0195a6ad252a205cc4b932
--- blog_frontend/blog_backend/social_auth/models.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> dae3be574253c406cd0195a6ad252a205cc4b932
