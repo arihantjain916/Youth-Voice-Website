46716d7300104aaa45af54158c1d381d25209d99
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-07-07 14:15:10 +0530

    Update index.jsx

--- blog_frontend/pages/blog/index.jsx
+++ blog_frontend/pages/blog/index.jsx
@@ -1,93 +1,115 @@
-import Link from "next/link";
 import LoginPopup from "../../components/common/form/login/LoginPopup";
 import FooterDefault from "../../components/footer/common-footer";
 import DefaulHeader from "../../components/header/DefaulHeader2";
 import Contact from "../../components/pages-menu/contact/ContactForm";
+import { useState, useEffect } from "react";
 
-export const getStaticProps = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/details/blogs/");
-  const data = await res.json();
+const data = ({ data }) => {
+  const [apiData, setApiData] = useState(data);
 
-  return {
-    props: {
-      data: data,
-    },
-    revalidate: 20,
-  };
-};
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://127.0.0.1:8000/api/details/blogs/");
+      const newData = await res.json();
+      setApiData(newData);
+    };
 
-const blog = ({ data }) => {
+    fetchData(); // Fetch data immediately
+
+    const interval = setInterval(fetchData, 20000); // Fetch data every 20 seconds
+
+    return () => {
+      clearInterval(interval); // Clean up the interval on component unmount
+    };
+  }, []);
+
   return (
     <>
       <span className="header-span"></span>
-
       <LoginPopup />
-
       <DefaulHeader />
-      {data?.map((data) => {
-        return (
-          <div
-            className="news-block-two col-lg-6 col-md-6 col-sm-12"
-            key={data.id}
-          >
-            <div className="content-box">
-              <ul className="post-meta">
-                <li>
-                  <a href="#">July 04, 23</a>
-                </li>
-                <li>
-                  <a href="#">12 Comment</a>
-                </li>
-              </ul>
-              <h3>
-                <Link href={`/blog/${data.slug}`}>{data.title}</Link>
-              </h3>
-              <p className="text">{data.content}</p>
-              <Link href={`/blog/${data.slug}`} className="read-more">
-                Read More <i className="fa fa-angle-right"></i>
-              </Link>
+
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {apiData?.map((data) => (
+            <div className="lg:w-1/3 p-4" key={data.id}>
+              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                  {data.title}
+                </h1>
+                <p className="leading-relaxed mb-3">
+                  {data.content.slice(0, 250)}
+                </p>
+                <a
+                  className="text-indigo-500 inline-flex items-center"
+                  href={`/blog/${data.slug}`}
+                >
+                  Learn More
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+                <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                  <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    1.2K
+                  </span>
+                  <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      class="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                    6
+                  </span>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    ></svg>
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          ))}
+        </div>
+      </div>
       <Contact />
       <FooterDefault footerStyle="alternate5" />
     </>
   );
 };
 
-export const threeblog = () => {
-  return (
-    <>
-      {data.slice(0, 3).map((data) => {
-        return (
-          <div
-            className="news-block-two col-lg-6 col-md-6 col-sm-12"
-            key={data.id}
-          >
-            <div className="content-box">
-              <ul className="post-meta">
-                <li>
-                  <a href="#">July 04, 23</a>
-                </li>
-                <li>
-                  <a href="#">12 Comment</a>
-                </li>
-              </ul>
-              <h3>
-                <Link href={`/blog/${data.slug}`}>{data.title}</Link>
-              </h3>
-              <p className="text">{data.content}</p>
-              <Link href={`/blog/${data.slug}`} className="read-more">
-                Read More <i className="fa fa-angle-right"></i>
-              </Link>
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
-};
-
-export default blog;
-// threeblog;
+export default data;
+// // threeblog;
