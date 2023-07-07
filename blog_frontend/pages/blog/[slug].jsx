5830fc9bd908d71ca653ef127da632bb1a3a4dcc
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-07-07 14:13:52 +0530

    Update [slug].jsx
    Change the CSS and update the axios

--- blog_frontend/pages/blog/[slug].jsx
+++ blog_frontend/pages/blog/[slug].jsx
@@ -6,26 +6,27 @@ import { useEffect, useState } from "react";
 import Seo from "../../components/common/Seo";
 import Contact from "../../components/pages-menu/contact/ContactForm";
 
-const BlogDetails = ({ conten }) => {
-  // const [plaintext, setPlainText] = useState("");
+const BlogDetails = () => {
   const router = useRouter();
   const { slug } = router.query;
   const [blog, setBlog] = useState(null);
+  const [blogFound, setBlogFound] = useState(true);
 
   useEffect(() => {
-    // const parser = new DOMParser();
-    // const doc = parser.parseFromString(conten, "text/html");
-    // const text = doc.body.textContent || "";
-    // setPlainText(text);
     const fetchBlogDetails = async () => {
       try {
         const res = await fetch(
           `http://127.0.0.1:8000/api/details/blogs/${slug}/`
         );
-        const data = await res.json();
-        setBlog(data);
+        if (res.ok) {
+          const data = await res.json();
+          setBlog(data);
+        } else {
+          setBlogFound(false);
+        }
       } catch (error) {
         console.error("Error fetching blog details:", error);
+        setBlogFound(false);
       }
     };
 
@@ -36,41 +37,49 @@ const BlogDetails = ({ conten }) => {
 
   return (
     <>
-      <Seo pageslug="Blog Details " />
+      <Seo pageTitle="Blog Details " />
 
-      {/* <!-- Header Span --> */}
       <span className="header-span"></span>
 
       <LoginPopup />
-      {/* End Login Popup Modal */}
 
       <DefaulHeader />
-      {/* <!-- End Blog Single --> */}
 
-      <div>
-        {blog ? (
-          <section className="blog-single">
-            <div className="auto-container">
-              <div className="upper-box">
-                <h3>{blog?.title}</h3>
+      {blogFound ? (
+        <>
+          {blog ? (
+            <section className="blog-single">
+              <div className="auto-container">
+                <div className="upper-box">
+                  <h3>{blog.title}</h3>
 
-                <ul className="post-info">
-                  <li>August 31, 2021</li>
-                  <li>12 Comment</li>
-                </ul>
-                <h2>{blog.content}</h2>
+                  <ul className="post-info">
+                    <li>August 31, 2021</li>
+                    <li>12 Comment</li>
+                  </ul>
+                  <h2>{blog.content}</h2>
+                </div>
               </div>
-            </div>
-          </section>
-        ) : (
-          <h1>No Blogs Found</h1>
-        )}
-      </div>
-      <a href="/blog">
-        <button className="theme-btn btn-style-one" type="submit" name="log-in">
-          Back
-        </button>
-      </a>
+            </section>
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </>
+      ) : (
+        <>
+          <h1>No Blog Found</h1>
+          <a href="/blog">
+            <button
+              className="theme-btn btn-style-one"
+              type="submit"
+              name="back"
+            >
+              Back
+            </button>
+          </a>
+        </>
+      )}
+
       <Contact />
       <FooterDefault footerStyle="alternate5" />
       {/* <!-- End Main Footer --> */}
