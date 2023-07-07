ea1c310a096d6906c0e7fcf0f02011a44a1cca44
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-07-07 14:16:08 +0530

    Update BlogFetch.jsx

--- blog_frontend/components/blog/BlogFetch.jsx
+++ blog_frontend/components/blog/BlogFetch.jsx
@@ -1,131 +1,10 @@
-// import Link from "next/link";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// // export const getStaticProps = async () => {
-// //   try {
-// //     const res = await fetch("http://127.0.0.1:8000/api/details/blogs/");
-// //     const data = await res.json();
-// //     console.log(res);
-
-// //     if (res.ok) {
-// //       console.log(data);
-// //     } else {
-// //       console.log("error");
-// //     }
-
-// //     return {
-// //       props: {
-// //         data: data,
-// //       },
-// //     };
-// //   } catch (error) {
-// //     console.error("Error fetching data:", error);
-// //     return {
-// //       props: {
-// //         data: [],
-// //       },
-// //     };
-// //   }
-// // };
-
-// const FetchBlog = () => {
-//   try {
-//     let response = axios
-//       .get("http://127.0.0.1:8000/api/details/blogs/")
-//       .then((response) => {
-//         const data = response.data;
-//         console.log(data);
-//         if (!Array.isArray(data))
-//           return (
-//             <>
-//               <h1>No data found</h1>
-//             </>
-//           );
-//         else {
-//           return (
-//             <>
-//               {data.slice(0, 3).map((data) => {
-//                 return (
-//                   <div
-//                     className="news-block-two col-lg-6 col-md-6 col-sm-12"
-//                     key={data.id}
-//                   >
-//                     <div className="content-box">
-//                       <ul className="post-meta">
-//                         <li>
-//                           <a href="#">July 04, 23</a>
-//                         </li>
-//                         <li>
-//                           <a href="#">12 Comment</a>
-//                         </li>
-//                       </ul>
-//                       <h3>
-//                         <Link href={`/blog/${data.slug}`}>{data.title}</Link>
-//                       </h3>
-//                       <p className="text">{data.content}</p>
-//                       <Link href={`/blog/${data.slug}`} className="read-more">
-//                         Read More <i className="fa fa-angle-right"></i>
-//                       </Link>
-//                     </div>
-//                   </div>
-//                 );
-//               })}
-//             </>
-//           );
-//         }
-//       });
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-// FetchBlog();
-
-// // // fetchapi();
-
-// // const Blog = ({ data }) => {
-// //   // const { blog } = data
-// //   console.log(data);
-// //   return (
-// //     <>
-// //       {data?.slice(0, 3).map((data) => {
-// //         return (
-// //           <div
-// //             className="news-block-two col-lg-6 col-md-6 col-sm-12"
-// //             key={data.id}
-// //           >
-// //             <div className="content-box">
-// //               <ul className="post-meta">
-// //                 <li>
-// //                   <a href="#">July 04, 23</a>
-// //                 </li>
-// //                 <li>
-// //                   <a href="#">12 Comment</a>
-// //                 </li>
-// //               </ul>
-// //               <h3>
-// //                 <Link href={`/blog/${data.slug}`}>{data.title}</Link>
-// //               </h3>
-// //               <p className="text">{data.content}</p>
-// //               <Link href={`/blog/${data.slug}`} className="read-more">
-// //                 Read More <i className="fa fa-angle-right"></i>
-// //               </Link>
-// //             </div>
-// //           </div>
-// //         );
-// //       })}
-// //     </>
-// //   );
-// // };
-
-// export default FetchBlog;
-// // Now the getStaticProps function properly returns the props object with the fetched data, and the Blog component can use it as intended. Additionally, I've added a try-catch block to handle any potential errors that may occur during the data fetching process.
-
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const FetchBlog = () => {
   const [blogs, setBlogs] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -151,30 +30,38 @@ const FetchBlog = () => {
       {blogs.length === 0 ? (
         <h1>No data found</h1>
       ) : (
-        blogs.map((blog) => (
-          <div
-            className="news-block-two col-lg-6 col-md-6 col-sm-12"
-            key={blog.id}
-          >
-            <div className="content-box">
-              <ul className="post-meta">
-                <li>
-                  <a href="#">July 04, 23</a>
-                </li>
-                <li>
-                  <a href="#">12 Comment</a>
-                </li>
-              </ul>
-              <h3>
-                <Link href={`/blog/${blog.slug}`}>{blog.title}</Link>
-              </h3>
-              <p className="text">{blog.content}</p>
-              <Link href={`/blog/${blog.slug}`} className="read-more">
-                Read More <i className="fa fa-angle-right"></i>
-              </Link>
-            </div>
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {blogs.map((blog) => (
+              <div key={blog.id} className="bg-gray-100 p-4 rounded-lg">
+                <h1 className="text-2xl font-medium mb-4">{blog.title}</h1>
+                <p className="mb-4">{blog.content.slice(0, 250)}</p>
+                <a
+                  href={`/blog/${blog.slug}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    router.push(`/blog/${blog.slug}`);
+                  }}
+                  className="text-indigo-500 inline-flex items-center"
+                >
+                  Learn More
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            ))}
           </div>
-        ))
+        </div>
       )}
     </>
   );
